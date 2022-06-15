029ac1f9bc4d89a93cbf9801085c72deb4eda8d2
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-15 19:03:41 +0200

    http common

--- ui/src/http-common.js
+++ ui/src/http-common.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-axios.get('http://localhost:8081')
+axios.get('https://ukrainelodgeservicecloud.azurewebsites.net')
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'https://ukrainelodgeservicecloud.azurewebsites.net/api',
   headers: {
     "Content-type": "application/json"
   }

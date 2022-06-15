091dedee51286326259143e9341b6660ccec037c
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-15 18:35:31 +0200

    db config

--- api/app/config/db.config.js
+++ api/app/config/db.config.js
@@ -1,13 +1,19 @@
+var fs = require('fs');
+
+
 module.exports = {
-    HOST: "ukrainelodgeservice.mysql.database.azure.com",
-    USER: "root",
-    PASSWORD: "Admin123456",
-    DB: "ukrainelodgeservice",
-    dialect: "mysql",
+    host: 'ukrainelodgeservice.mysql.database.azure.com',
+    user: 'user1',
+    password: 'Admin123456',
+    database: 'ukrainelodgeservice',
+    port: 3306,
+    dialect: 'mysql',
+    ssl: { ca: fs.readFileSync("C:/Users/Sara/Downloads/DigiCertGlobalRootCA.crt.pem") },
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
-    }
+    },
+    dialect: 'mysql',
 };
--- api/app/models/index.js
+++ api/app/models/index.js
@@ -1,7 +1,8 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
+var fs = require('fs');
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
     dialect: dbConfig.dialect,
     operatorsAliases: false,
     pool: {
@@ -9,6 +10,10 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
         min: dbConfig.pool.min,
         acquire: dbConfig.pool.acquire,
         idle: dbConfig.pool.idle
+    },
+    ssl: { ca: fs.readFileSync("C:/Users/Sara/Downloads/DigiCertGlobalRootCA.crt.pem") },
+    dialectOptions: {
+        ssl: { ca: fs.readFileSync("C:/Users/Sara/Downloads/DigiCertGlobalRootCA.crt.pem") }
     }
 });
 const db = {};
